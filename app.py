2993bc64fcc680f1cdc2f6eecf2aa6e3942c2c29
GuGuGaGa-Star <nik.21daren@gmail.com>
2025-03-15 15:22:39 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from pygame import *
+from random import randint
+
+
 
 img_back = "galaxy.jpg"
 img_hero = "rocket.png"
+img_enemy = "ufo.png"
+img_bullet = "bullet.png"
 
 
 win_width = 700
@@ -20,10 +25,15 @@ mixer.music.load('space.ogg')
 mixer.music.play(-1) #повтор трека
 fire_sound = mixer.Sound('fire.ogg')
 
+font.init()
+font2 = font.Font(None, 36)
 
+score = 0
+lost = 0
+goal = 20
+max_lost = 5
 
 class GameSprite(sprite.Sprite):
-
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
@@ -47,15 +57,36 @@ class Player(GameSprite):
 
 
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
+class Enemy(GameSprite):
 
+    def update(self):
+        self.rect.y += self.speed
+        global lost
 
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
+class Bullet(GameSprite):
+
+    def update(self):
+        self.rect.y += self.speed
+
+        if self.rect.y < 0:
+            self.kill()
 
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
 
-
+bullets = sprite.Group()
 
 while run:
 
@@ -63,10 +94,26 @@ while run:
         if e.type == QUIT:
             run = False
 
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+
+
     if not finish:
         window.blit(background, (0, 0))
+        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
         ship.update()
         ship.reset()
+        monsters.update()
+        monsters.draw(window)
+        bullets.update()
+        bullets.draw(window)
 
 
     display.update()
